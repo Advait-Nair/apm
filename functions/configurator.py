a28b3a47471bcb494695ba77f8e33a7578aa3696
Advait Nair <advaitincorporated@gmail.com>
2023-11-09 20:25:45 +0000

    updated to utilise shells .rc

--- functions/configurator.py
+++ functions/configurator.py
@@ -1,13 +1,32 @@
 from functions.spectrum import success, error, italic
 import os
+
+
+def exists(filePath):
+    try:
+        open(filePath,'r')
+        return True
+    except:
+        return False
+
+
+_zsh='.zshrc'
+_bash='.bashrc'
+
+if not exists('~/'+_zsh):
+    open('~/'+_zsh, 'n')
+if not exists('~/'+_bash):
+    open('~/'+_bash, 'n')
+
+
 zpc="""\n\nalias apm=~/apm/apm.sh
 alias apmdev=~/apm/apmdev.sh
-alias zpr="cd ~ && source .zprofile"\n
-"""
+alias zpr="cd ~ && source {_zsh}"\n
+""".format(_zsh=_zsh)
 bpc="""\n\nalias apm=~/apm/apm.sh
 alias apmdev=~/apm/apmdev.sh
-alias zpr="cd ~ && source .bash_profile"\n
-"""
+alias zpr="cd ~ && source {_bash}"\n
+""".format(_bash=_bash)
 baseLoc = os.getcwd()
 if 'apm' in baseLoc:
     baseLoc = baseLoc.replace('apm', '')
@@ -23,41 +42,41 @@ print(baseLoc)
 def config_zprofile():
     zprofile = None
     try:
-        zprofile = open(baseLoc+'.zprofile','r').read().replace(zpc, '').replace(bpc, '')
+        zprofile = open(baseLoc+_zsh,'r').read().replace(zpc, '').replace(bpc, '')
     except:
-        print(error('PROCESS: ZPROFILE CONFIG FAIL: Likely profile does not exist.'))
+        print(error('PROCESS: ZSH CONFIG FAIL: Likely profile does not exist.'))
         return
     try:
-        print('PROCESS: ZPROFILE CONFIG')
-        open(baseLoc+'.zprofile','w').write(zprofile+zpc)
-        print(success('PROCESS: ZPROFILE CONFIG SUCCESS'))
+        print('PROCESS: ZSH CONFIG')
+        open(baseLoc+_zsh,'w').write(zprofile+zpc)
+        print(success('PROCESS: ZSH CONFIG SUCCESS'))
     except:
-        print(error('PROCESS: ZPROFILE CONFIG ERROR'))
-        open(baseLoc+'.zprofile','w').write(zprofile)
+        print(error('PROCESS: ZSH CONFIG ERROR'))
+        open(baseLoc+_zsh,'w').write(zprofile)
 
 def config_bash_profile():
     bash_profile = None
     try:
-        bash_profile = open(baseLoc+'.bash_profile','r').read().replace(zpc, '').replace(bpc, '')
+        bash_profile = open(baseLoc+_bash,'r').read().replace(zpc, '').replace(bpc, '')
     except:
-        print(error('PROCESS: BASH_PROFILE CONFIG FAIL: Likely profile does not exist.'))
+        print(error('PROCESS: BASH CONFIG FAIL: Likely profile does not exist.'))
         return
     try:
-        print('PROCESS: BASH_PROFILE CONFIG')
-        open(baseLoc+'.bash_profile','w').write(bash_profile+bpc)
-        print(success('PROCESS: BASH_PROFILE CONFIG SUCCESS'))
+        print('PROCESS: BASH CONFIG')
+        open(baseLoc+_bash,'w').write(bash_profile+bpc)
+        print(success('PROCESS: BASH CONFIG SUCCESS'))
     except:
-        print(error('PROCESS: BASH_PROFILE CONFIG ERROR'))
-        open(baseLoc+'.bash_profile','w').write(bash_profile)
+        print(error('PROCESS: BASH CONFIG ERROR'))
+        open(baseLoc+_bash,'w').write(bash_profile)
 
 def config_apmshell(np):
     print('PROCESS: APM.SH CONFIG')
-    replacing = '.zprofile'
+    replacing = _zsh
     replacebin = 'bash'
     newbin = 'zsh'
-    if np == '.zprofile':
-        replacing = '.bash_profile'
-    elif np == '.bash_profile':
+    if np == _zsh:
+        replacing = _bash
+    elif np == _bash:
         replacebin = 'zsh'
         newbin = 'bash'
 
